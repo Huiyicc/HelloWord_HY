ff24a159eb1eb40271b8ebd2d1397abf33dfc279
Huiyicc <1925473403@qq.com>
2024-01-14 01:34:57 +0800

    upd: - 更新构建模板

--- HelloWord-Dynamic-fw/UserApp/build_info.h
+++ HelloWord-Dynamic-fw/UserApp/build_info.h
@@ -2,12 +2,21 @@
 #define BUILD_INFO_H
 
 // 构建时间
-#define BUILD_TIMESTAMP 20240106
+#define BUILD_TIMESTAMP 20240114
+constexpr const int build_timestamp = BUILD_TIMESTAMP;
+// 构建版本
+#define BUILD_VERSION "v2024.01.14"
+constexpr const char* build_version = BUILD_VERSION;
+// 构建版本标志
+#define BUILD_VERSION_PARSE_TAG "build_version_parse_tag:{{v2024.01.14}}:build_version_parse_tag"
+constexpr const char* build_version_parse_tag = BUILD_VERSION_PARSE_TAG;
+
+
 
 // 构建版本
 #define GIT_VERSION ""
 // 构建哈希
-#define GIT_HASH "915931c"
+#define GIT_HASH "80c2b37"
 // 构建分支
 #define GIT_BRANCH "dev"
 
